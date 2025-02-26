8705f8d65f10c04ec78c0512c4751ae37ac16d00
Radhakrishna Vempati <vempatir@uci.edu>
2025-02-25 19:23:00 -0800

    start game button added

---
+++ src/components/StartGameButton.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface StartGameButtonProps {
+  onClick: () => void;
+}
+
+const StartGameButton: React.FC<StartGameButtonProps> = ({ onClick }) => {
+  return (
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: "100vh" }}
+    >
+      <button onClick={onClick} className="btn btn-info btn-lg shadow-lg">
+        Start Game
+      </button>
+    </div>
+  );
+};
+
+export default StartGameButton;
